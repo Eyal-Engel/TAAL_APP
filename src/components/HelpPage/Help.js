5279fb87a0b3f767bd3a2fc4c87dfc121ca2fcf2
Eyal Engel <108540831+Eyal-Engel@users.noreply.github.com>
2022-10-19 12:47:30 +0300

    update link

--- src/components/HelpPage/Help.js
+++ src/components/HelpPage/Help.js
@@ -202,12 +202,13 @@ function Help(props) {
                 <a
                   href={
                     phoneGuide !== ""
-                      ? "https://wa.me/" +
-                        phoneGuide +
-                        "?text=" +
-                        "התקשיתי במילוי המשימות שלי. אשמח לסיוע ותודה על הרצון לעזור" +
-                        "&amp;app_absent=0"
-                      : "Error Phone doesn't exists"
+                      ? "https://api.whatsapp.com/send/?phone=972502996949&text=%D7%94%D7%AA%D7%A7%D7%A9%D7%99%D7%AA%D7%99+%D7%91%D7%9E%D7%99%D7%9C%D7%95%D7%99+%D7%94%D7%9E%D7%A9%D7%99%D7%9E%D7%95%D7%AA+%D7%A9%D7%9C%D7%99.+%D7%90%D7%A9%D7%9E%D7%97+%D7%9C%D7%A1%D7%99%D7%95%D7%A2+%D7%95%D7%AA%D7%95%D7%93%D7%94+%D7%A2%D7%9C+%D7%94%D7%A8%D7%A6%D7%95%D7%9F+%D7%9C%D7%A2%D7%96%D7%95%D7%A8&type=phone_number&app_absent=0"
+                      : // "https://wa.me/" +
+                        //   phoneGuide +
+                        //   "?text=" +
+                        //   "התקשיתי במילוי המשימות שלי. אשמח לסיוע ותודה על הרצון לעזור" +
+                        //   "&amp;app_absent=0"
+                        "Error Phone doesn't exists"
                   }
                 >
                   <button className="helpNow" onClick={toggleModal}>
